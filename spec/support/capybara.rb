d4f004becb9289521a31490beecf71e1096e4997
Irina_Githubkina <lepekhina-irina@yandex.ru>
2023-05-25 10:12:32 -0600

    Added configuration for JavaScript and Capybara tests in Docker with SeleniumChrome

---
+++ spec/support/capybara.rb
@@ -0,0 +1,27 @@
+# spec/support/capybara.rb
+
+# Register the Selenium Chrome driver for running tests in a container
+Capybara.register_driver :selenium_chrome_in_container do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--disable-dev-shm-usage') # Disable /dev/shm usage for improved compatibility in containers
+  options.add_argument('--no-sandbox') # Disable the sandbox for better compatibility in containers
+
+  Capybara::Selenium::Driver.new app,
+    browser: :remote,
+    url: "http://selenium_chrome:4444/wd/hub", # URL of the remote Selenium Chrome container
+    options: options
+end
+
+# Register the headless Selenium Chrome driver for running tests in a container
+Capybara.register_driver :headless_selenium_chrome_in_container do |app|
+  options = Selenium::WebDriver::Remote::Capabilities.chrome(
+    'goog:chromeOptions' => {
+      'args' => %w[headless disable-gpu] # Run Chrome in headless mode and Disable GPU acceleration
+    }
+  )
+
+  Capybara::Selenium::Driver.new app,
+    browser: :remote,
+    url: "http://selenium_chrome:4444/wd/hub", # URL of the remote Selenium Chrome container
+    options: options
+end
